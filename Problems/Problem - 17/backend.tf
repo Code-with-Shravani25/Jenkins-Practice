6a854a6c718570cb3fd4a4dd1aa7f5b91ec40a69
Shravani Budharam <shravanibudharam25@gmail.com>
2026-06-29 00:14:51 +0530

    Delete Problems/Problem - 17 directory

--- Problems/Problem - 17/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-locking-state-file-practice"
-    key            = "ec2-project/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
--- Problems/Problem - 17/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-ami_id =  "ami-091138d0f0d41ff90"
-instance_type = "t2.medium"
-key_name = "Demo"
